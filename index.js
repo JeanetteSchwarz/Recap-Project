c95c4f4d986a9396825ceded54b69976d45df012
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-01-25 10:51:57 +0100

    add answer visibilty

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmarkActive");
 });
+
+//Answer-Button///
+
+const answerButton = document.querySelector('[data-js="answerButton"]');
+const answerElement = document.querySelector('[data-js="answerElement"]');
+
+/* console.log(answerButton);
+console.log(answerElement); */
+
+answerButton.addEventListener("click", () => {
+  answerElement.classList.toggle("hidden");
+});
